b498df568b90e2457491accd32d5c26a7386b08c
ushka1 <dawid.prajzner01@gmail.com>
2023-08-01 22:08:12 +0200

    fix logs

--- src/shared/services/socket.ts
+++ src/shared/services/socket.ts
@@ -1,13 +1,17 @@
 import { io } from 'socket.io-client';
 
-export const socket = io(process.env.SOCKET_ENDPOINT);
+export const socket = io(process.env.SOCKET_ENDPOINT, {
+  query: {
+    userId: '123',
+  },
+});
 
 socket.on('connect', () => {
   console.log(`User connected (socket.id=${socket.id}).`);
 });
 
 socket.on('disconnect', () => {
-  console.log('User disconnected.');
+  console.log(`User disconnected (socket.id=${socket.id}).`);
 });
 
 type Message = {
@@ -16,9 +20,9 @@ type Message = {
 };
 
 socket.on('notification-channel', (notification: Message) => {
-  console.log('notification:', notification);
+  console.log('Notification received:', notification);
 });
 
 socket.on('error-channel', (error: Message) => {
-  console.log('error:', error);
+  console.log('Error received:', error);
 });
